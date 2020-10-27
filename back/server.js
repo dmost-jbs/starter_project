307f9c470e159c0f1b0e94c614dd6ac6683f3844
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-10-27 15:06:57 -0400

    some routing on express

--- back/server.js
+++ back/server.js
@@ -7,5 +7,12 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.get('/api/message', function (req, res) {
+    res.send('get api endpoint');
+});
+
+app.post('/api/message', function (req, res) {
+    res.send('post api endpoint');
+});
 
 var server = app.listen(5000, function () { });
