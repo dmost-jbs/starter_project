3171a469d375130ef78058c96c9d949784bc7183
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-10-27 13:17:18 -0400

    implementing axios functionality

--- front/src/main_display/user_interface.js
+++ front/src/main_display/user_interface.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
-import FormCheck from 'react-bootstrap/FormCheck'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
-
+import axios from 'axios';
 
 const handleSubmit = event => {
 	event.preventDefault();
+    const user = {
+      name: this.state.name
+    }
+    axios.post('https://jsonplaceholder.typicode.com/users', { user })
+      .then(res=>{
+        console.log(res);
+        console.log(res.data);
+        window.location = "/retrieve" //This line of code will redirect you once the submission is succeed
+      })
 }
+
+
 
 const Interface = () => {
 	return (
 		<div>
-			<Container>
-				<Row className="justify-content-md-center">
-					<Col md="auto">
-						<Form>
+			<Form onSubmit = {handleSubmit}>
+				<Container>
+					<Row className="justify-content-md-center">
+						<Col md="auto">
 							<Form.Group controlId="formBasicInput">
 								<Form.Control type="text" placeholder="Input string" />
 							</Form.Group>
-						</Form>
-					</Col>
-				</Row>
-				<Row className="justify-content-md-center">
-					<Col md="auto">
-						<Button variant="primary" type="submit">
-							Submit
-  							</Button>
-					</Col>
-					<Col md="auto">
-						<Button variant="primary" type="submit">
-							Retrieve
-  							</Button>
-					</Col>
-				</Row>
-			</Container>
-
-
+						</Col>
+					</Row>
+					<Row className="justify-content-md-center">
+						<Col md="auto">
+							<Button variant="primary" type="submit">
+								Submit
+							</Button>
+						</Col>
+						<Col md="auto">
+							<Button variant="primary" type="submit">
+								Retrieve
+							</Button>
+						</Col>
+					</Row>
+				</Container>
+			</Form>
 		</div>
 	)
 }
