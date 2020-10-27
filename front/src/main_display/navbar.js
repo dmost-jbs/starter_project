71262753eaa30af772b11d213c5dc894bda1a46a
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-10-27 12:16:10 -0400

    init navbar and imports bootstrap

---
+++ front/src/main_display/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+
+
+const NavigationBar = () => {
+	  return (
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+            Home
+        </Navbar.Brand>
+        </Navbar>
+	  )
+}
+ 
+export default NavigationBar;
+
+
+
