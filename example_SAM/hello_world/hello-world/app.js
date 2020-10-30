40b06c54dce1008a55573f56e68ff6896eb5571a
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-10-30 14:50:08 -0400

    inital boilerplate for reading s3 from lambda

--- example_SAM/hello_world/hello-world/app.js
+++ example_SAM/hello_world/hello-world/app.js
@@ -2,6 +2,15 @@
 // const url = 'http://checkip.amazonaws.com/';
 let response;
 
+
+// dependencies
+const AWS = require('aws-sdk');
+const util = require('util');
+const sharp = require('sharp');
+
+// get reference to S3 client
+const s3 = new AWS.S3()
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,12 +21,26 @@ let response;
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ * test
  */
 exports.lambdaHandler = async (event, context) => {
     try {
         console.log(event)
         console.log(context)
+        var src_bkt = event.Records[0].s3.bucket.name;
+        var src_key = event.Records[0].s3.object.key;
+        let test = s3.getObject({
+        Bucket: src_bkt,
+        Key: src_key
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err);
+        } else {
+            console.log("Raw text:\n" + data.Body.toString('ascii'));
+            callback(null, null);
+        }
+        console.log(test)
         // const ret = await axios(url);
         if (event.httpMethod == 'POST') {
             response = {
