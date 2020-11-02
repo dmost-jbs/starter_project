ba0de5c71f339c0eef33f829a7482bacabf22a53
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-02 15:13:55 -0500

    styling

--- front/src/login/login.js
+++ front/src/login/login.js
@@ -14,66 +14,66 @@ const Login = () => {
     const [passwordText, setPasswordText] = useState("");
 
 
-	/**
-	 * Handles login attempts to the app
-	 * @param {object} event 
-	 */
-	const handleSubmit = event => {
+    /**
+     * Handles login attempts to the app
+     * @param {object} event 
+     */
+    const handleSubmit = event => {
         event.preventDefault();
-        
+
         // login logic here
 
-	}
-
-
-	/**
-	 * Handles updates to the username text form
-	 * @param {object} event object from inputting text into the input form
-	 */
-	const handleUsername = event => {
-		setUsernameText(event.target.value);
     }
-    
+
+
     /**
-	 * Handles updates to the password text form
-	 * @param {object} event object from inputting text into the input form
-	 */
-	const handlePassword = event => {
-		setPasswordText(event.target.value);
-	}
+     * Handles updates to the username text form
+     * @param {object} event object from inputting text into the input form
+     */
+    const handleUsername = event => {
+        setUsernameText(event.target.value);
+    }
+
+    /**
+     * Handles updates to the password text form
+     * @param {object} event object from inputting text into the input form
+     */
+    const handlePassword = event => {
+        setPasswordText(event.target.value);
+    }
 
 
-	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<Container>
-					<Row className="justify-content-md-center" style={{ marginBottom: 30 }}>
-                        <Col md="auto">
-							<p>username:</p>
-						</Col>
-						<Col md="auto">
-							<input type="text" onChange={handleUsername} value={usernameText} />
-						</Col>
-					</Row>
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <Container>
                     <Row className="justify-content-md-center" style={{ marginBottom: 30 }}>
                         <Col md="auto">
-							<p>password:</p>
-						</Col>
-						<Col md="auto">
-							<input type="text" onChange={handlePassword} value={passwordText} />
-						</Col>
-					</Row>
-					<Row className="justify-content-md-center">
-						<Col md="auto">
-							<Button variant="primary" type="submit">
-								Login
+                            <p>username:</p>
+                        </Col>
+                        <Col md="auto">
+                            <input type="text" onChange={handleUsername} value={usernameText} />
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center" style={{ marginBottom: 30 }}>
+                        <Col md="auto">
+                            <p>password:</p>
+                        </Col>
+                        <Col md="auto">
+                            <input type="text" onChange={handlePassword} value={passwordText} />
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <Button variant="primary" type="submit">
+                                Login
 							</Button>
-						</Col>
-					</Row>
-				</Container>
-			</form>
-		</div>
-	)
+                        </Col>
+                    </Row>
+                </Container>
+            </form>
+        </div>
+    )
 }
 
 export default Login;
