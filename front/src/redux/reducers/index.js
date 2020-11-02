4390fb1d472b8581c58796a8344b38dde8ed72f4
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-02 16:41:24 -0500

    registration reducer - render account data ui tomorrowish

--- front/src/redux/reducers/index.js
+++ front/src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import logoutReducer from "./logoutReducer"
+import registerReducer from "./registerReducer"
 
 
-export default combineReducers({loginReducer,logoutReducer});
+export default combineReducers({loginReducer,logoutReducer,registerReducer});
