f7882a8c46868769c490aca67d361e25ff6bd4c8
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-02 10:47:16 -0500

    refactoring and commenting

--- example_SAM/hello_world/hello-world/app.js
+++ example_SAM/hello_world/hello-world/app.js
@@ -22,85 +22,62 @@ const s3 = new AWS.S3()
  * test
  */
 exports.lambdaHandler = async (event, context) => {
+    response = {
+        'statusCode': 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+        }
+    }
     try {
-        // const ret = await axios(url);
+        // handles post requests
         if (event.httpMethod == 'POST') {
-            let Bucket = 'most-starter';
-            let Key = 'test.txt';
-            let message = JSON.parse(event.body)['message']
 
+            let message = JSON.parse(event.body)['message'] // message gathered from post request
             let params = {
-                Body: message,
-                Bucket: 'most-starter',
-                Key: "message.txt"
+                Body: message, // text to go inside bucket object
+                Bucket: 'most-starter', // bucket name
+                Key: "message.txt" // object name within bucket
             };
             try {
-                const in_data = await s3.putObject(params).promise();
-                response = {
-                    'statusCode': 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Headers": "Content-Type",
-                        "Access-Control-Allow-Methods": "OPTIONS,GET"
-                    },
-                    'body': JSON.stringify({
+                const in_data = await s3.putObject(params).promise(); // puts object in bucket
+                response['body']= JSON.stringify({
                         'message': message,
                         'ETag': in_data['ETag'],
-                    })
-                }
+                });
+                return response;
             }
             catch (err) {
-                response = {
-                    'statusCode': 500,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Headers": "Content-Type",
-                        "Access-Control-Allow-Methods": "OPTIONS,GET"
-                    },
-                    'body': JSON.stringify({
-                        message: err.toString()
-                        // location: ret.data.trim()
-                    })
-                }
+                response['body']= JSON.stringify({
+                    'message':  err.toString(),
+                });
+                return response;
             }
-            return response;
         }
+        // handles get requests
         if (event.httpMethod == 'GET') {
-            let Bucket = 'most-starter';
-            let Key = 'message.txt';
+            let Bucket = 'most-starter'; // bucket name
+            let Key = 'message.txt'; // object name in bucket
             try {
-                const data = await s3.getObject({ Bucket, Key }).promise();
-                response = {
-                    'statusCode': 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Headers": "Content-Type",
-                        "Access-Control-Allow-Methods": "OPTIONS,GET"
-                    },
-                    'body': JSON.stringify({
-                        message: data.Body.toString('ascii'),
-                        // location: ret.data.trim()
-                    })
-                }
+                const data = await s3.getObject({ Bucket, Key }).promise(); // get the object from s3
+                response['body']= JSON.stringify({
+                    'message':  data.Body.toString('ascii'),
+                });
                 return response
             }
             catch (err) {
-                response = {
-                    'statusCode': 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Headers": "Content-Type",
-                        "Access-Control-Allow-Methods": "OPTIONS,GET"
-                    },
-                    'body': JSON.stringify({
-                        message: 'Error reading latest text',
-                        // location: ret.data.trim()
-                    })
-                }
+                response['body']= JSON.stringify({
+                    'message':  'Error reading latest text',
+                });
+                return response;
             }
         }
     }
     catch (err) {
-        console.log(err);
+        response['body']= JSON.stringify({
+            'message':  err.toString(),
+        });
+        return response;
     }
 };
