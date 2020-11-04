ecc0abba1c30068c087eb4ae361e57cfffed5b68
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-04 15:29:07 -0500

    adding roles to stack

--- server/lib/server-stack.ts
+++ server/lib/server-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as s3 from '@aws-cdk/aws-s3'
+import * as s3 from '@aws-cdk/aws-s3';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import * as iam from '@aws-cdk/aws-iam';
+
 
 export class ServerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -17,6 +19,11 @@ export class ServerStack extends cdk.Stack {
       versioned: true
     });
     textBucket.grantRead(textApi);
+
+    const role = new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')   // required
+    });
+
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: textApi
