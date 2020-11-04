0b201843aee58fcf6e5324175349eba41265c7af
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-04 10:17:16 -0500

    initializing s3

--- template/bin/template.js
+++ template/bin/template.js
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
-
-const cdk = require('@aws-cdk/core');
+const core = require('@aws-cdk/core');
+const s3 = require('@aws-cdk/aws-s3');
 const { TemplateStack } = require('../lib/template-stack');
 
 const app = new cdk.App();
 new TemplateStack(app, 'TemplateStack');
+
+//init api
+
+
+//init s3
+
+
+class HelloCdkStack extends core.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+    
+    new s3.Bucket(this, 'most_learning_project', {
+      versioned: true
+    });
+  }
+}
+
+module.exports = { HelloCdkStack }
+
+//init lambda
+
+//settings lambda->s3
+
+
+
