cc4d7369e8a88c38e46256c821e0f7f83c1eb514
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-04 13:19:52 -0500

    lambda build

--- server/lib/server-stack.ts
+++ server/lib/server-stack.ts
@@ -2,9 +2,16 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class ServerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const textApi = new lambda.Function(this,'TextApiHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'textApi.handler'
+    })
   }
 }
