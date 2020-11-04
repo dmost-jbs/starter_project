ec8739140d5a3e704ebc4b0cf211a964cc045c2c
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-04 13:10:40 -0500

    unused learning projects

--- template/bin/template.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const core = require('@aws-cdk/core');
-const { TemplateStack } = require('../lib/template-stack');
-
-const app = new core.App();
-new TemplateStack(app, 'TemplateStack');
