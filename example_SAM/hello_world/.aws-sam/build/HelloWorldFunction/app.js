d087885a1dcbd835588c8dc2a0f1757d95ca557e
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-04 13:11:21 -0500

    example sam stuff

--- example_SAM/hello_world/.aws-sam/build/HelloWorldFunction/app.js
+++ example_SAM/hello_world/.aws-sam/build/HelloWorldFunction/app.js
@@ -2,6 +2,13 @@
 // const url = 'http://checkip.amazonaws.com/';
 let response;
 
+
+// dependencies
+const AWS = require('aws-sdk');
+
+// get reference to S3 client
+const s3 = new AWS.S3()
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,22 +19,53 @@ let response;
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ * test
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+        console.log(event)
+        console.log(context)
+        var Bucket = 'most-starter';
+        var Key = 'test.txt';
 
-    return response
+        // const ret = await axios(url);
+        if (event.httpMethod == 'POST') {
+            var params = {
+                Body: "test test test string", 
+                Bucket: 'most-starter', 
+                Key: "test_object.jpg"
+               };
+               s3.putObject(params, function(err, data) {
+                // an error occurred
+                 if (err) {
+                     console.log(err, err.stack); 
+                 }
+                // successful response
+                 else{
+                    console.log(data);   
+                 }             
+            });
+            response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message: 'post endpoint',
+                    // location: ret.data.trim()
+                })
+            }
+        }
+        if (event.httpMethod == 'GET') {
+            const data = await s3.getObject({ Bucket, Key }).promise();
+            response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message: data.Body.toString('ascii'),
+                    // location: ret.data.trim()
+                })
+            }
+            return response
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 };
