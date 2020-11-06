26318ba6159a961c8bf0997449ff3a00effeed75
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-06 11:04:06 -0500

    working through tsc errors

--- server/lambda/lambdaHandler.ts
+++ server/lambda/lambdaHandler.ts
@@ -1,6 +1,7 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 let response;
+declare var require: any;
+declare var exports: any;
+
 
 
 // dependencies
@@ -8,9 +9,8 @@ const AWS = require('aws-sdk');
 
 // get reference to S3 client
 const s3 = new AWS.S3()
-exports.handler = async function(event) {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-      response = {
+exports.handler = async function (event: Object) {
+    response = {
         'statusCode': 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
@@ -18,10 +18,9 @@ exports.handler = async function(event) {
             "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
         }
     }
-  try {
+    try {
         // handles post requests
         if (event.httpMethod == 'POST') {
-
             let message = JSON.parse(event.body)['message'] // message gathered from post request
             let params = {
                 Body: message, // text to go inside bucket object
@@ -30,15 +29,15 @@ exports.handler = async function(event) {
             };
             try {
                 const in_data = await s3.putObject(params).promise(); // puts object in bucket
-                response['body']= JSON.stringify({
-                        'message': message,
-                        'ETag': in_data['ETag'],
+                response['body'] = JSON.stringify({
+                    'message': message,
+                    'ETag': in_data['ETag'],
                 });
                 return response;
             }
             catch (err) {
-                response['body']= JSON.stringify({
-                    'message':  err.toString(),
+                response['body'] = JSON.stringify({
+                    'message': err.toString(),
                 });
                 return response;
             }
@@ -49,23 +48,31 @@ exports.handler = async function(event) {
             let Key = 'message.txt'; // object name in bucket
             try {
                 const data = await s3.getObject({ Bucket, Key }).promise(); // get the object from s3
-                response['body']= JSON.stringify({
-                    'message':  data.Body.toString('ascii'),
+                response['body'] = JSON.stringify({
+                    'message': data.Body.toString('ascii'),
                 });
                 return response
             }
             catch (err) {
-                response['body']= JSON.stringify({
-                    'message':  'Error reading latest text',
+                response['body'] = JSON.stringify({
+                    'message': 'Error reading latest text',
                 });
                 return response;
             }
         }
-    }
-    catch (err) {
-        response['body']= JSON.stringify({
-            'message':  err.toString(),
+        response['body'] = JSON.stringify({
+            'message': 'invalid http request method',
         });
         return response;
     }
+    catch (err) {
+        response['body'] = JSON.stringify({
+            'message': err.toString(),
+        });
+        return response;
+    }
+    response['body'] = JSON.stringify({
+        'message': 'server error',
+    });
+    return response;
 }
