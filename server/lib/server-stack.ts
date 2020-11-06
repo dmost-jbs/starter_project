df1bbc19910846d819b8173e6d581b21a17f38f6
dmost-jbs <73480874+dmost-jbs@users.noreply.github.com>
2020-11-06 10:39:59 -0500

    eslint stuff and migration to TS from JS on lambda

--- server/lib/server-stack.ts
+++ server/lib/server-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as apigw from '@aws-cdk/aws-apigateway';
-import * as iam from '@aws-cdk/aws-iam';
 
 
 export class ServerStack extends cdk.Stack {
